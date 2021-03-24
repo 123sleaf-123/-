e85b30887f1bce053733cab3310822db52f0b3c5
sleaf <80097831+123sleaf-123@users.noreply.github.com>
2021-03-25 02:12:52 +0800

    真正的作业3
    可能已经很完美

--- 作业3.c
+++ 作业3.c
@@ -15,18 +15,22 @@ int FormatInput(char opt)
 			}
 			else
 			{
+				printf("\n\n\n\n\n\n\n\n\n\n");
 				return 0;
 				break;
 			}
 		}
 		else
 		{
-			getchar();
+			while(getchar() != 10);// delete meaningless strings
 			printf ("\n=============================\n       INVALID DATA ! \n=============================\n");
 			printf("Enter valid data (Y or N) to make your choice.\n");
 			printf("\nYour choice:");
 			scanf("%c",&opt);
+			//getchar();
+			//printf("Reloading main component...waiting......\n");
 		}
+		//getchar();//delete \n
 	}
 }
 
@@ -34,7 +38,7 @@ int main (void)
 {
 	int num1,num2,num3;
 	double average;
-	int check,i;
+	int i;
 	char opt;
 
 	
@@ -47,36 +51,27 @@ int main (void)
 		scanf("%d",&num2);
 		printf("Enter 3rd integer numbers: ");
 		scanf("%d",&num3);
-		while(1)
-		{
-			check = scanf("%c",&opt);
-			if (check == 1)
-			{
-				getchar();
-			}
-			else
-			{
-				goto jump;
-			}
-		}
-		jump:
+		while(getchar() != 10);// delete meaningless strings
+		//getchar();//delete \n
 		printf("%c",7);// prompt sound rings when enter finished
 		printf("\n=========== Data has been successfully entered! ===========\n");
 		printf("\nThe 1st number is %d.\nThe 2nd number is %d.\nThe 3rd number is %d.\n",num1, num2, num3);
 		printf("\n=========== Data has been successfully entered! ===========\n");
 		printf("\nAre these numbers what you need?");
 		printf("(Y or N)\n");
-		getchar();
 		printf("\nYour choice:");
 		scanf("%c",&opt);
 		i=FormatInput(opt);
 	}while (i == 0);
 	
+	printf("\n\n=========== Data enter finished! ===========\n\n");
+	printf("%c",7);
+	
 	/* calculate the average */
 	average = (num1+num2+num3) / 3.0;
 	
 	/* display the result */
-	printf("\nThe average of %d, %d and %d is %f\n",num1, num2, num3, average);
+	printf("\nThe average of %d, %d and %d is %f\n\n",num1, num2, num3, average);
 	
 	return 0;
 }
